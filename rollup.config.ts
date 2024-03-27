782115e6693a4366b6e94b5e0bcc99efcbcd73f3
sergey.krasnov <sergey.krasnov@truelab.games>
2024-03-27 15:46:30 +0300

    fix: tsconfig changed

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,13 +1,13 @@
 import typescript from '@rollup/plugin-typescript';
 import { defineConfig } from 'rollup';
+import flatDts from 'rollup-plugin-flat-dts';
+
+const tsconfig = 'tsconfig.build.json';
 
 export default defineConfig([
   {
     input: 'src/index.ts',
-    output: {
-      file: 'dist/index.js',
-      format: 'es',
-    },
-    plugins: [typescript({ tsconfig: './tsconfig.build.json' })],
+    output: { file: 'dist/index.js', format: 'es', sourcemap: true },
+    plugins: [typescript({ tsconfig }), flatDts({ tsconfig, compilerOptions: { declarationMap: true } })],
   },
 ]);
